3970b2f8535bd105128120f8a9ff6d01dd91c141
Swarnim Jain <swarnimjain004@gmail.com>
2024-08-17 16:41:49 +0100

    Add ThreadPool class for managing a pool of threads

---
+++ src/common/thread_pool.h
@@ -0,0 +1,26 @@
+// src/common/thread_pool.h
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <condition_variable>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
+    void enqueue(std::function<void()> task);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    std::mutex queueMutex;
+    std::condition_variable condition;
+    bool stop;
+};
+
+#endif // THREAD_POOL_H
